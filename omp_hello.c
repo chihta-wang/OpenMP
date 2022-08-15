ab3ae8a6eb0d4873e8d1065f53a4afcc8a45a2b2
Chih-Ta Wang <wangchihta@gmail.com>
2022-08-16 00:03:18 +0200

    Add error-checking functionality

--- omp_hello.c
+++ omp_hello.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#ifdef _OPENMP
 #include <omp.h>
+#endif
 
 void Hello(void); /* thread function */
 
 int main(int argc, char* argv[])
-{   /* Get the number of threads from command line */
+{   
+    if (argc != 2)
+    {
+        printf("Enter the number of threads you want to create!\n");
+        exit(1);
+    }
+
+    /* Get the number of threads from command line */
     int thread_count = strtol(argv[1], NULL, 10);
+    if (thread_count <= 0)
+    {
+        printf("The number of threads should be positive!\n");
+        exit(1);
+    }
 
 # pragma omp parallel num_threads(thread_count)
     Hello();
@@ -16,8 +31,12 @@ int main(int argc, char* argv[])
 
 void Hello(void)
 {
+#ifdef _OPENMP
     int my_rank = omp_get_thread_num();
     int thread_count = omp_get_num_threads();
-
+#else
+    int my_rank = 0;
+     int thread_count = 1;
+#endif
     printf("Hello from %d of %d\n", my_rank, thread_count);
 }   /* Hello */
